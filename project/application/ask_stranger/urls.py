e8087360f2b1fe4e4c0aaaecd26ea7e664e17a55
Arthur <aptyp.potatoch@gmail.com>
2019-11-04 22:56:03 +0300

    full commit

--- project/application/ask_stranger/urls.py
+++ project/application/ask_stranger/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from . import views
 
+app_name = 'ask_stranger'
 urlpatterns = [
-        path('',       views.index,            name = 'index'),
-        path('hot/',    views.hot,              name = 'hot'),
-        path('tag/',    views.tagged,           name = 'tagged'),
-        path('question/',   views.question,     name = 'question'),
+        path('base/', views.base, name='base'),
+
+        path('index/<int:page>', views.index, name = 'index'),
+        path('',                 views.index, name = 'index'),
+
+        path('hot/', views.hot,              name = 'hot'),
+        path('tag/', views.tagged,           name = 'tagged'),
+
+        path('question/<int:question_id>',   views.question,     name = 'question'),
         path('login/',  views.login,            name = 'login'),
         path('signup/', views.signup,           name = 'signup'),
         path('ask/',    views.ask,              name = 'ask'),
--- project/application/ask_stranger/views.py
+++ project/application/ask_stranger/views.py
@@ -1,20 +1,79 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-# Create your views here.
+def base(request):
+    return render(request, 'ask_stranger/base.html', {})
+
+def paginate(objects, page, objects_on_page = 10):
+    paginator = Paginator(objects, objects_on_page)
+    return paginator.get_page(page)
 
 
 def index(request, page = 1):
-    return HttpResponse("index")
+    questions = []
+    for i in range(50):
+        questions.append({
+            'title': 'title ' + str(i),
+            'id'  : i,
+            'text': 'teeeeext' + str(i),
+    })
+
+    questions = paginate(questions, page)
+
+    return render(request, 'ask_stranger/index.html', {'questions':questions})
+
 def hot(request, page = 1):
-    return HttpResponse("hot")
+    for i in range(50):
+        questions.append({
+            'title': 'title ' + str(i),
+            'id'  : i,
+            'text': 'teeeeext' + str(i),
+    })
+
+    objects = paginate(objects, page)
+
+
+    return render(request, 'ask_stranger/hot.html', {'questions':questions})
+
+
 def tagged(request, tag = 'def', page = 1):
-    return HttpResponse("tagged")
-def question(request, qu_id='def'):
-    return HttpResponse("question")
+    for i in range(50):
+        questions.append({
+            'title': 'title ' + str(i),
+            'id'  : i,
+            'text': 'teeeeext' + str(i),
+    })
+
+    objects = paginate(objects, page)
+
+    return render(request, 'ask_stranger/tagged.html', {'questions':questions})
+
+
+def question(request, question_id):
+    # question = get_object_or_404(Question, pk = question_id)
+    # answers  = get_list_or_404(Answer, question_id = question_id)
+
+    question = {
+            'title': 'title' + str(question_id),
+            'id'  : 'id ' + str(question_id),
+            'text': 'teeeeext' + str(question_id),
+    }
+
+    answers = []
+    for i in range(10):
+        answers.append({
+            'user': 'user' + str(i),
+            'text': 'answer' + str(i),
+        })
+
+    return render(request, 'ask_stranger/question.html', {'question':question,'answers': answers})
+
 def login(request):
-    return HttpResponse("login")
+    return render(request, 'ask_stranger/login.html', {})
+
 def signup(request):
-    return HttpResponse("signup")
+    return render(request, 'ask_stranger/signup.html', {})
+
 def ask(request):
-    return HttpResponse("ask")
+    return render(request, 'ask_stranger/ask.html', {})
